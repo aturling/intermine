323bc8ebf28e393d1102f593bc21d6965a075d6e
Amy Walsh <aturling@users.noreply.github.com>
2022-07-06 15:09:00 -0500

    Customized Reactome source

--- bio/sources/reactome/src/main/java/org/intermine/bio/dataconversion/ReactomeConverter.java
+++ bio/sources/reactome/src/main/java/org/intermine/bio/dataconversion/ReactomeConverter.java
@@ -32,9 +32,10 @@ import org.intermine.xml.full.Item;
 
 
 /**
- *
+ * Modified from original to load Pathway.organism reference.
  *
  * @author Julie Sullivan
+ * @author
  */
 public class ReactomeConverter extends BioFileConverter
 {
@@ -91,7 +92,7 @@ public class ReactomeConverter extends BioFileConverter
                 // invalid organism
                 continue;
             }
-            Item pathway = getPathway(pathwayIdentifier, pathwayName);
+            Item pathway = getPathway(pathwayIdentifier, pathwayName, taxonId);
 
             Item protein = getProtein(accession, taxonId);
             protein.addToCollection("pathways", pathway);
@@ -131,12 +132,13 @@ public class ReactomeConverter extends BioFileConverter
     }
 
 
-    private Item getPathway(String pathwayId, String pathwayName) throws ObjectStoreException {
+    private Item getPathway(String pathwayId, String pathwayName, String taxonId) throws ObjectStoreException {
         Item item = pathways.get(pathwayId);
         if (item == null) {
             item = createItem("Pathway");
             item.setAttribute("identifier", pathwayId);
             item.setAttribute("name", pathwayName);
+            item.setReference("organism", getOrganism(taxonId));
             pathways.put(pathwayId, item);
         }
         return item;
