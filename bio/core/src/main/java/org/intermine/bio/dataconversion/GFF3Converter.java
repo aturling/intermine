12f4b7109b3b3c297e31c5550964bde116e83386
Amy Walsh <aturling@users.noreply.github.com>
2022-10-25 14:29:24 -0500

    Added loadSequenceAlterations flag to GFF loader

--- bio/core/src/main/java/org/intermine/bio/dataconversion/GFF3Converter.java
+++ bio/core/src/main/java/org/intermine/bio/dataconversion/GFF3Converter.java
@@ -64,6 +64,7 @@ public class GFF3Converter extends DataConverter
     private GFF3SeqHandler sequenceHandler;
     private boolean dontCreateLocations;
     private boolean loadDuplicateEntities = false;
+    private boolean loadSequenceAlterations = false;
     private final Map<String, Item> dataSets = new HashMap<String, Item>();
     private final Map<String, Item> dataSources = new HashMap<String, Item>();
 
@@ -807,6 +808,23 @@ public class GFF3Converter extends DataConverter
      */
     public boolean getLoadDuplicateEntities() {
         return this.loadDuplicateEntities;
+    }
+
+    /**
+     * Set the loadSequenceAlterations flag
+     * @param loadSequenceAlterations loadSequenceAlterations flag
+     */
+    public void setLoadSequenceAlterations(boolean loadSequenceAlterations) {
+        System.out.println("Setting loadSequenceAlterations to " + loadSequenceAlterations);
+        this.loadSequenceAlterations = loadSequenceAlterations;
+    }
+
+    /**
+     * Return the value of loadSequenceAlterations flag
+     * @return boolean loadSequenceAlterations flag
+     */
+    public boolean getLoadSequenceAlterations() {
+        return this.loadSequenceAlterations;
     }
 
     /**
--- bio/core/src/main/java/org/intermine/bio/task/GFF3ConverterTask.java
+++ bio/core/src/main/java/org/intermine/bio/task/GFF3ConverterTask.java
@@ -54,6 +54,7 @@ public class GFF3ConverterTask extends Task
 
     private boolean dontCreateLocations = false;
     private boolean loadDuplicateEntities = false;
+    private boolean loadSequenceAlterations = false;
 
      /**
      * Set the data fileset
@@ -115,6 +116,18 @@ public class GFF3ConverterTask extends Task
             this.loadDuplicateEntities = true;
         } else {
             this.loadDuplicateEntities = false;
+        }
+    }
+
+    /**
+     * Set whether to load Sequence Alterations as flank markers
+     * @param loadSequenceAlterations whether to load Sequence Alterations
+     */
+    public void setLoadSequenceAlterations(String loadSequenceAlterations) {
+        if ("true".equalsIgnoreCase(loadSequenceAlterations)) {
+            this.loadSequenceAlterations = true;
+        } else {
+            this.loadSequenceAlterations = false;
         }
     }
 
@@ -270,6 +283,9 @@ public class GFF3ConverterTask extends Task
             }
             if (loadDuplicateEntities) {
                 gff3converter.setLoadDuplicateEntities(loadDuplicateEntities);
+            }
+            if (loadSequenceAlterations) {
+                gff3converter.setLoadSequenceAlterations(loadSequenceAlterations);
             }
 
             for (int i = 0; i < files.length; i++) {
